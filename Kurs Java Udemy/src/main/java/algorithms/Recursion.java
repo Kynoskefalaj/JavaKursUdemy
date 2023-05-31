b724619b6b259bd07dc1be5c596f7904e7a72f15
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-06-01 00:02:25 +0200

    Recursion more examples

--- Kurs Java Udemy/src/main/java/algorithms/Recursion.java
+++ Kurs Java Udemy/src/main/java/algorithms/Recursion.java
@@ -6,7 +6,11 @@ public class Recursion{
 
     public static void main(String[] args) {
         int[] myArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println("" + sum(myArr));
+        System.out.println("Sum is:" + sum(myArr));
+
+        System.out.println("Array length is: " + count(myArr));
+
+        System.out.println("Maximum value is: " + max(myArr));
     }
 
     static int sum(int[] arr){
@@ -14,6 +18,28 @@ public class Recursion{
             return 0;
         } else {
             return arr[0] + sum(Arrays.copyOfRange(arr, 1, arr.length));
+        }
+    }
+
+    static int count(int[] arr){
+        if (arr.length == 0){
+            return 0;
+        } else {
+            return 1 + count(Arrays.copyOfRange(arr, 1, arr.length));
+        }
+    }
+
+    static int max(int[] arr){
+        if (arr.length == 2){
+            if (arr[0] > arr[1]){
+                return arr[0];
+            } else
+                return arr[1];
+        } else {
+            if (arr[0] > max(Arrays.copyOfRange(arr, 1, arr.length))){
+                return arr[0];
+            } else
+                return max(Arrays.copyOfRange(arr, 1, arr.length));
         }
     }
 }
