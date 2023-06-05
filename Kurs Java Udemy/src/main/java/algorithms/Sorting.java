765c606a0049528cee35cf89430e507e7fd5e475
Kynoskefalaj <91989136+Kynoskefalaj@users.noreply.github.com>
2023-06-05 23:26:52 +0200

    Sorting class

---
+++ Kurs Java Udemy/src/main/java/algorithms/Sorting.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+public class Sorting {
+
+    public static void main(String[] args) {
+    }
+
+
+    void printArray(int[] arr){
+        for (int number : arr) {
+            System.out.println(number);
+        }
+    }
+
+    int[] bubbleSort(int[] arr) {
+        int[] sortedArr = new int[10];
+        return sortedArr;
+    }
+}
